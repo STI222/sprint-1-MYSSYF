02a85103ebeab0c99895b5b5042ebbc892e4a7ab
MYSSYF <jogabolu@gmail.com>
2022-09-01 09:55:47 -0500

    version 2

--- crime.py
+++ crime.py
@@ -1,6 +1,8 @@
 import pandas as pan
+print(pan.__version__ +" pandas")
 import numpy as num
-
+print(num.__version__ +" numpy")
+print("---------------------------------------------")
 db = pan.read_csv("./war.csv")
 new = db.sort_values("tiempo", ascending=False)
 pip = db.sort_values("crimenes", ascending=True)
